c695e2417e977d2cb7a4c67eb3440f0aba946d09
i.kotelnikov <i.kotelnikov@timeweb.ru>
2023-12-02 15:32:17 +0300

    fix validation to create method

--- dtos/create_user_dto.go
+++ dtos/create_user_dto.go
@@ -1,7 +1,7 @@
 package dto
 
 type CreateUserDto struct {
-	Name       string `json:"name" binding:"required" validate:"required,min=1,max=30"`
-	Secondname string `json:"secondname" binding:"required" validate:"required,min=1,max=30"`
+	Name       string `json:"name" binding:"required,min=1,max=30"`
+	Secondname string `json:"secondname" binding:"required,min=1,max=30"`
 	Age        int    `json:"age" binding:"required,min=1,max=99"`
 }
